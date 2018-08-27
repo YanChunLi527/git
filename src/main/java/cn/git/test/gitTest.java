16e77d3799e505bbe40536c799a71f0b30cfdb29
yanchunli <yanchunli527@gmail.com>
2018-08-27 18:39:18 +0800

    更改

--- src/main/java/cn/git/test/gitTest.java
+++ src/main/java/cn/git/test/gitTest.java
@@ -2,7 +2,7 @@ package cn.git.test;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.println("今天吃屎了吗？");
+        System.out.println("今？");
         System.out.println("jdj");
     }
 }
